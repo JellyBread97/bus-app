4573c9d8f77acb1f143eff93ec8562d0fbfe472b
MartinKotas <kotasmartin1997@gmail.com>
2023-04-19 22:10:48 +0100

    adjust color in tw config from gray to slate

--- apps/lp/tailwind.config.js
+++ apps/lp/tailwind.config.js
@@ -1,7 +1,23 @@
+const colors = require("tailwindcss/colors")
+
 module.exports = {
   darkMode: "class",
   content: ["./src/**/*.{html,js,ts,tsx}"],
   theme: {
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      black: colors.black,
+      white: colors.white,
+      gray: colors.slate,
+      green: colors.green,
+      purple: colors.purple,
+      yellow: colors.yellow,
+      pink: colors.pink,
+      red: colors,
+      red,
+      blue: colors.blue,
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",

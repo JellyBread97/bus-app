de29b27e8819251d7c228a9228870c8cd818f247
MartinKotas <kotasmartin1997@gmail.com>
2023-04-29 00:49:50 +0100

    minor fix

--- apps/lp/src/data/request.ts
+++ apps/lp/src/data/request.ts
@@ -8,15 +8,13 @@ export const request = async ({ url, method = "GET", params }) => {
 
     const endpoint = apiUrl + route
 
-    await fetch(endpoint, {
-      method,
-    })
-      .then(async response => {
-        const result = await response.json()
-        resolve(result)
-      })
-      .catch(error => {
-        // catch error
-      })
+    try {
+      const response = await fetch(endpoint, { method })
+      const result = await response.json()
+      resolve(result)
+    } catch (error) {
+      console.error(`Error fetching ${url}:`, error)
+      reject(error)
+    }
   })
 }
